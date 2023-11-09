da2d619a0b4cc609e108c1c343c1385559551ad5
Phuong Thanh Nguyen <s43pnguy@uni-bonn.de>
2023-11-09 10:02:26 +0100

    intensity check

---
+++ preprocess.py
@@ -0,0 +1,41 @@
+from skimage.transform import resize
+import os
+from multiprocessing import pool
+import numpy as np
+import nibabel as nib
+
+def resize_image(image, old_spacing, new_spacing, order=3):# -> Any:
+    new_shape = (int(np.round(old_spacing[0]/new_spacing[0]*float(image.shape[0]))),
+                 int(np.round(old_spacing[1]/new_spacing[1]*float(image.shape[1]))),
+                 int(np.round(old_spacing[2]/new_spacing[2]*float(image.shape[2]))))
+    return resize(image, new_shape, order=order, mode='edge')
+
+def convert_to_one_hot(seg):
+    vals = np.unique(seg)
+    res = np.zeros([len(vals)] + list(seg.shape), seg.dtype)
+    for c in range(len(vals)):
+        res[c][seg == c] = 1
+    return res
+
+
+def preprocess_image(nib_image, spacing_target, is_seg=False, keep_z_spacing=False):
+    spacing = np.array(nib_image.header.get_zooms())[[2, 1, 0]]
+    image = nib_image.get_fdata()
+    if keep_z_spacing:
+        spacing_target = list(spacing_target)
+        spacing_target[0] = spacing[0]
+    if not is_seg:
+        order_img = 3
+        if not keep_z_spacing:
+            order_img = 1
+        image = resize_image(image, spacing, spacing_target, order=order_img).astype(np.float32)
+        image -= image.mean()
+        image /= image.std()
+    else:
+        tmp = convert_to_one_hot(image)
+        vals = np.unique(image)
+        results = []
+        for i in range(len(tmp)):
+            results.append(resize_image(tmp[i].astype(float), spacing, spacing_target, 1)[None])
+        image = vals[np.vstack(results).argmax(0)]
+    return image
