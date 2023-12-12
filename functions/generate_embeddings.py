6af6c6f32965348a1b6dee5251522f43e73b9328
Lisa Krapp <s6likrap@uni-bonn.de>
2023-12-12 13:00:58 +0100

    draft of script

--- functions/generate_embeddings.py
+++ functions/generate_embeddings.py
@@ -4,3 +4,20 @@ import nibabel as nib
 import sys
 import glob
 import gzip
+from segment_anything import SamPredictor, sam_model_registry
+import torch
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+MODEL_TYPE = "vit_h"
+CHECKPOINT_PATH = "sam_vit_h_4b8939.pth"
+sam = sam_model_registry[MODEL_TYPE](checkpoint=CHECKPOINT_PATH)
+sam.to(device=DEVICE)
+
+# loop through all images in preprocess
+z_stack_embedding = []
+# loop through z-stacks
+predictor = SamPredictor(sam)
+predictor.set_image(image)
+embedding = predictor.get_image_embedding()
+z_stack_embedding.append(embedding)
+# save z stack embedding in the folder for each patient with _emb at the end of the filename
